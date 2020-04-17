39aed77bc11362483427d1cbce414817b7c44655
ramosgerald255 <37137949+ramosgerald255@users.noreply.github.com>
2020-04-17 10:28:01 +0800

    Recent changes

--- BlobStorage/Form1.Designer.vb
+++ BlobStorage/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,13 +20,15 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.btnBrowse = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtPath = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnUpload = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -49,13 +51,16 @@ Partial Class Form1
         Me.btnBrowse.Text = "Browse Image"
         Me.btnBrowse.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtPath
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 245)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(322, 84)
-        Me.TextBox1.TabIndex = 2
+        Me.txtPath.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPath.Location = New System.Drawing.Point(12, 245)
+        Me.txtPath.Multiline = True
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtPath.Size = New System.Drawing.Size(322, 84)
+        Me.txtPath.TabIndex = 2
         '
         'Label1
         '
@@ -79,14 +84,32 @@ Partial Class Form1
         Me.btnUpload.Text = "Upload to Azure"
         Me.btnUpload.UseVisualStyleBackColor = False
         '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(12, 360)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 34)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "View Cloud Files"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(347, 406)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnUpload)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtPath)
         Me.Controls.Add(Me.btnBrowse)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
@@ -99,7 +122,9 @@ Partial Class Form1
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents btnBrowse As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtPath As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents btnUpload As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
 End Class
--- BlobStorage/Form1.vb
+++ BlobStorage/Form1.vb
@@ -1,3 +1,104 @@
-﻿Public Class Form1
+﻿'Imports System.IO
+'Imports Microsoft.WindowsAzure.Storage
+'Imports Microsoft.WindowsAzure.Storage.Blob
+Imports System
+Imports System.IO
+Imports System.Threading.Tasks
+'Imports Microsoft.Azure.Storage
+'Imports Microsoft.Azure.Storage.Blob
+Imports Microsoft.WindowsAzure.Storage
+Imports Microsoft.WindowsAzure.Storage.Auth
+Imports Microsoft.WindowsAzure.Storage.Blob
+Imports System.Configuration
+Imports System.Threading
+
+Public Class Form1
+
+
+    Dim doesBrowsing As Boolean = False
+    Dim accountName As String = "mkprojectmanagement"
+    Dim accessKey As String = "8Akkcxur0kMh6CIlHhQhzD0618VT1P4u1ZRDHx7hnzlZOpo0ZWmSXMFUtPfmn50brHYXAv+mGc2zUR5yL+elHQ=="
+
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        OpenFileDialog1.Filter = "Bitmap |*.bmp| JPG | *.jpg | GIF | *.gif | All Files|*.*"
+        OpenFileDialog1.FileName = ""
+
+        If OpenFileDialog1.ShowDialog(Me) = DialogResult.OK Then
+
+            'Get the image name 
+
+            Dim img As String = OpenFileDialog1.FileName
+
+            'Create a new Bitmap and display it 
+
+            PictureBox1.Image = System.Drawing.Bitmap.FromFile(img)
+            txtPath.Text = img
+            doesBrowsing = True
+
+
+        End If
+    End Sub
+
+    Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        Select Case doesBrowsing
+            Case False
+                MsgBox("Browse some picture first")
+            Case True
+                'MsgBox("I'm uploading now")
+                If Upload(txtPath.Text) = True Then
+                    MsgBox("Uploaded Sucessfully")
+                End If
+        End Select
+    End Sub
+
+    Public Function Upload(ByVal filename As String) As Boolean
+        Try
+
+
+
+            Dim connectionString As String = "DefaultEndpointsProtocol=https;AccountName=mkprojectmanagement;AccountKey=8Akkcxur0kMh6CIlHhQhzD0618VT1P4u1ZRDHx7hnzlZOpo0ZWmSXMFUtPfmn50brHYXAv+mGc2zUR5yL+elHQ==;EndpointSuffix=core.windows.net"
+            Dim containerName As String = "uploads"
+
+
+            Dim storageAccount As CloudStorageAccount = CloudStorageAccount.Parse(connectionString)
+            Dim blobClient As CloudBlobClient = storageAccount.CreateCloudBlobClient()
+            Dim container As CloudBlobContainer = blobClient.GetContainerReference(containerName)
+            Dim blockBlob As CloudBlockBlob = container.GetBlockBlobReference(Path.GetFileName(filename).ToString)
+
+
+            'blockBlob.UploadFromFileAsync(filename)
+
+            'If blockBlob.Properties.Length >= 0 Then
+            '    Dim blobUrl As String = blockBlob.Uri.AbsoluteUri
+            '    MsgBox("Uploaded: " & blobUrl)
+
+            '    Dim blobs = blockBlob.Container.ListBlobs
+            '    MsgBox("Blobs: " & blobs.ToString)
+            '    Return True
+            'End If
+            Using FileStream = System.IO.File.OpenRead(filename)
+                blockBlob.UploadFromStream(FileStream)
+                Return True
+            End Using
+
+            'DOWNLOAD File FROM SERVER
+            'Dim NewBlob2 As CloudBlockBlob = container.GetBlockBlobReference("10838-03-04-2014--19-12-32.pdf")
+            'NewBlob2.DownloadToFile("c:\Folder\LocalFilename.pdf", IO.FileMode.CreateNew)
+
+
+        Catch ex As Exception
+            Return False
+            MsgBox(ex.Message)
+        End Try
+    End Function
+
+
+
+
+
+
+
+
 
 End Class
